6d61d68dd2d3eed161137960dc17c9d3df52aea9
NgocAnhDT <70968535+NgocAnhDT@users.noreply.github.com>
2020-11-28 09:44:39 +0700

    Hello

--- app/src/main/java/com/hus/oop_classroom/Login.java
+++ app/src/main/java/com/hus/oop_classroom/Login.java
@@ -1,13 +1,19 @@
 package com.hus.oop_classroom;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,42 +25,73 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     private EditText edit_email, edit_pw;
-    public static final String USERNAME = "USERNAME";
     private FirebaseAuth mAuth;
     private Users users;
+    CheckBox remember;
     TextView t1;
     Button login;
     String email, pw;
+    private SharedPreferences mPrefs;
+    private SharedPreferences.Editor editor;
+    private Boolean saveLogin;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         edit_email = (EditText) findViewById(R.id.edit_email);
         edit_pw = (EditText) findViewById(R.id.edit_pw);
-        mAuth= FirebaseAuth.getInstance();
         t1= (TextView) findViewById(R.id.lnkRegister);
+        login= (Button) findViewById(R.id.btnlogin);
+        remember= (CheckBox) findViewById(R.id.remember);
+        mAuth= FirebaseAuth.getInstance();
+        mPrefs= getSharedPreferences("checkbox",MODE_PRIVATE);
+        editor= mPrefs.edit();
+
+        //Get the user name and password data stored in the preferences
+        //Put the data we stored in the two input boxes
+        saveLogin = mPrefs.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            edit_email.setText(mPrefs.getString("email", ""));
+            edit_pw.setText(mPrefs.getString("pass", ""));
+            remember.setChecked(true);
+        }
 
         t1.setOnClickListener(new View.OnClickListener() {
-                                  @Override
-                                  public void onClick(View v) {
-                                      startActivity(new Intent(Login.this, Register.class));
-                                  }
-                              }
-        );
-        login= (Button) findViewById(R.id.btnlogin);
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                email =edit_email.getText().toString();
-                pw=edit_pw.getText().toString();
+                email =edit_email.getText().toString().trim();
+                pw=edit_pw.getText().toString().trim();
                 if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)){
+                    edit_email.setError("required");
+                    edit_pw.setError("required");
                     return;
                 }
                 if (!email.contains("@")) {
                     return;
+                }
+
+                if(view== login) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(edit_email.getWindowToken(), 0);
+                    if (remember.isChecked()) {
+                        editor.putBoolean("saveLogin", true);
+                        editor.putString("email", email);
+                        editor.putString("pass", pw);
+                        editor.commit();
+                    } else {
+                        editor.clear();
+                        editor.commit();
+                    }
                 }
                 mAuth.signInWithEmailAndPassword(email,pw).
                         addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
